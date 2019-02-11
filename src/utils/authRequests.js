be2a7cef23265290525343655b812a530164208e
ntmonov <ntm@mail.bg>
2019-02-11 11:33:21 +0200

    added authRequests

---
+++ src/utils/authRequests.js
@@ -0,0 +1,52 @@
+import { BASE_URL, APP_KEY, APP_SECRET, MASTER_SECRET, USER_ROLE_ID } from './config'
+
+const basicCredentials = 'Basic ' + window.btoa(APP_KEY + ':' + APP_SECRET)
+
+async function register(username, password, email, address) {
+  const response = await window.fetch(`${BASE_URL}user/${APP_KEY}`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': basicCredentials
+    },
+    body: JSON.stringify({ username, password,address, email })
+  })
+  return response.json()
+}
+
+async function login(username, password) {
+  const response = await window.fetch(`${BASE_URL}user/${APP_KEY}/login`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': basicCredentials
+    },
+    body: JSON.stringify({ username, password })
+  })
+  return response.json()
+}
+
+async function assignRole(userId) {
+  const credentials = 'Basic ' + window.btoa(APP_KEY + ':' + MASTER_SECRET)
+
+  const response = await window.fetch(`${BASE_URL}user/${APP_KEY}/${userId}/roles/${USER_ROLE_ID}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': credentials
+    }
+  })
+  return response.json()
+}
+
+async function logout() {
+  const credentials = 'Kinvey ' + window.sessionStorage.getItem('authToken')
+  await window.fetch(`${BASE_URL}user/${APP_KEY}/_logout`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': credentials
+    }
+  });}
+
+export { register, login, assignRole, logout }
--- src/utils/config.js
+++ src/utils/config.js
@@ -1,5 +1,7 @@
 const BASE_URL = 'https://baas.kinvey.com/'
 const APP_KEY = 'kid_S1Rwjn04V'
 const APP_SECRET = '3aeaf50a506c444e91f177f2b7358fd8'
+const MASTER_SECRET = '06d69d9d6f2844199754bf7bcc0a5bce'
+const USER_ROLE_ID = '87ed1129-b21e-40ee-bcd3-59f4e459470c'
 
-export default { BASE_URL, APP_KEY, APP_SECRET }
+export { BASE_URL, APP_KEY, APP_SECRET, MASTER_SECRET, USER_ROLE_ID }
