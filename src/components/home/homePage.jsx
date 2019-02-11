085641b723a8535096b22f262967930aa377d578
ntmonov <ntm@mail.bg>
2019-02-11 10:36:19 +0200

    added home page

---
+++ src/components/home/homePage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function HomePage () {
+  return (
+    <div className='jumbotron jumbotron-fluid bg-primary'>
+      <div className='container'>
+        <h1 className='display-4'>Welcome to Computer Heaven</h1>
+        <p className='lead'>This app is all about computers</p>
+      </div>
+    </div>
+  )
+}
+
+export default HomePage
