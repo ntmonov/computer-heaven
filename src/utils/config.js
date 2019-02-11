390517fc7590ec73d250c1517e3a04583c578339
ntmonov <ntm@mail.bg>
2019-02-11 11:09:47 +0200

    added config.js

---
+++ src/utils/config.js
@@ -0,0 +1,5 @@
+const BASE_URL = 'https://baas.kinvey.com/'
+const APP_KEY = 'kid_S1Rwjn04V'
+const APP_SECRET = '3aeaf50a506c444e91f177f2b7358fd8'
+
+export default { BASE_URL, APP_KEY, APP_SECRET }
