083ebbe2aa34bd3f0501379dc78e14ac761ba45c
ntmonov <ntm@mail.bg>
2019-04-22 14:48:18 +0300

    search request refactored

--- src/utils/searchRequests.js
+++ src/utils/searchRequests.js
@@ -2,7 +2,7 @@ import { BASE_URL, APP_KEY } from './config'
 import { get } from './crud'
 
 function getSearchCatalog (type, searchName, minPrice, maxPrice) {
-  return get(`${BASE_URL}appdata/${APP_KEY}/${type}?query={"$and":[{"price":{"$gte":${minPrice}}}, {"price":{"$lte":${maxPrice}}}, {"name":{"$regex":"^${searchName}"}}]}`, 'kinvey')
+  return get(`${BASE_URL}appdata/${APP_KEY}/${type}?query={"$and":[{"price":{"$gte":"${minPrice}"}}, {"price":{"$lte":"${maxPrice}"}}, {"name":{"$regex":"^${searchName}"}}]}`, 'kinvey')
 }
 
 export { getSearchCatalog }
